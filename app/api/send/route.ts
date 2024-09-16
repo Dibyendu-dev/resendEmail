c33be01067e71bbe255601074105d27874479566
ddas4548 <ddas4548@gmail.com>
2024-09-16 13:11:30 +0530

    resend  email templete in next.js project

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,16 +1,16 @@
-import { EmailTemplate } from '../../../components/email-template';
+import { GithubAccessTokenEmail } from '../../../components/email-template';
 import { Resend } from 'resend';
 import * as React from 'react';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function GET() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
+      from: 'Admin <onboarding@resend.dev>',
+      to: ['ddas4548@gmail.com'],
       subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+      react: GithubAccessTokenEmail({ username: 'ddas4548@gmail.com' }) as React.ReactElement,
     });
 
     if (error) {
